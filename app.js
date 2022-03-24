2b2fd2ae98900c78eb2b9f76d3749e014d0f35d9
Tonny-anjuman <tonnyara333@gmail.com>
2022-03-24 16:45:18 +0600

    setJs

---
+++ app.js
@@ -0,0 +1,39 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controlls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function PageTransitions() {
+  //Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", clickFunction);
+  }
+
+  //sections active
+  allSections.addEventListener("click", (e) => {
+    console.log(e);
+    const id = e.target.dataset.id;
+    console.log(id);
+    if (id) {
+      //remove seleted from the other btns
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("avtive");
+
+      //hide other section
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+    }
+  });
+}
+
+function clickFunction() {
+  let currentBtn = document.querySelectorAll(".active-btn");
+  currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
+  this.className += " active-btn";
+}
+
+PageTransitions();
